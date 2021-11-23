0bd03dd0851a755cb3fbddb6f2e6b1b08fe868e5
admin <admin@TeamCity>
2021-11-23 22:40:27 +0000

    TeamCity change in 'Dotnet Hello World' project: parameters of 'Compile' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -53,9 +53,9 @@ object Compile : BuildType({
         param("team_city_build_queue_endpoint", "/app/rest/buildQueue")
         password("team_city_stage_auth_token", "credentialsJSON:be2340ee-fa95-4882-8491-4013dfaa46e1")
         param("team_city_stage_build_config_id", "DotnetHelloWorld_Compile")
-        param("team_city_stage_server", "http://teamcity-server:8111")
         param("team_city_auth_endpoint", "/authenticationTest.html?csrf")
         param("env.RELEASE_NUMBER", "")
+        param("teamcity.stage.server", "http://teamcity-server:8111")
     }
 
     vcs {
