79953cc712ec5ae042ce84112968921befee4d55
admin <admin@TeamCity>
2021-11-23 01:57:17 +0000

    TeamCity change in 'Dotnet Hello World' project: parameters of 'Compile' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -54,7 +54,6 @@ object Compile : BuildType({
         password("BearerToken", "credentialsJSON:be2340ee-fa95-4882-8491-4013dfaa46e1")
         param("BuildConfigurationId", "DotnetHelloWorld_Compile")
         param("env.RELEASE_NUMBER", "")
-        param("env.SESSION_TOKEN", "test")
     }
 
     vcs {
