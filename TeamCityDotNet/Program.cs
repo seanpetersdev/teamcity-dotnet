9758e8584e9f0e123298684e0e71532c2186e43a
Sean Peters <sean.peters@xero.com>
2021-12-09 23:43:43 +1100

    AWS Parameter Store

--- TeamCityDotNet/Program.cs
+++ TeamCityDotNet/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using System;
+
 
 namespace TeamCityDotNet
 {
@@ -6,7 +9,28 @@ namespace TeamCityDotNet
     {
         static void Main(string[] args)
         {
+            string connectionString = null;
             Console.WriteLine("Hello World!");
+
+            var request = new GetParameterRequest()
+            {
+                Name = "seansparamtest"
+            };
+            using (var client = new AmazonSimpleSystemsManagementClient(Amazon.RegionEndpoint.GetBySystemName("ap-southeast-2")))
+            {
+                try
+                {
+                    var response = client.GetParameterAsync(request).GetAwaiter().GetResult(); connectionString = response.Parameter.Value;
+                    connectionString = response.Parameter.Value;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.WriteLine("String is: " + connectionString);
         }
     }
+
 }
